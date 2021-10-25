d6b495856ab69bc3c816741412b62cd27c14239f
Jack-rm <jackcnsr94@gmail.com>
2021-10-25 19:04:01 +0200

    Work on comics page using config data

--- resources/views/comics.blade.php
+++ resources/views/comics.blade.php
@@ -3,9 +3,25 @@
 @section('title', 'Comics')
 
 @section('content')
-    <div>
-        <h1>Comics Page test</h1>
+<div id="comics">
+
+    <div class="my_content">
+
+        @foreach ($comics as $comic)
+        
+        <div class="my_comic-element">
+            <div class="my_comic-img">
+                <img src="{{ $comic['thumb'] }}" alt="{{ $comic['series'] }}">
+            </div>
+            <h3>{{ $comic['title'] }}</h3>
+        </div>
+
+        @endforeach
+
     </div>
+
+    <button class="my_btn-light">Load More</button>
+</div>
 @endsection
 
 <!-- 
