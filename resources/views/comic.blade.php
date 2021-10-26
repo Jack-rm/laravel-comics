a92709091fc10d748f92c97981be40df1999aef4
Jack-rm <jackcnsr94@gmail.com>
2021-10-26 18:07:42 +0200

    Comic blade html/css structure

--- resources/views/comic.blade.php
+++ resources/views/comic.blade.php
@@ -27,12 +27,44 @@
                     <p>Available</p>
                 </div>
                 <div class="comic-check">
-                    <p>Check Availability</p>
+                    <p>Check Availability <</p>
                 </div>
             </div>
-            <p> Descrizione Fumetto</p>
+            <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quibusdam, ea impedit. Enim voluptatum, sapiente minima eveniet nostrum accusamus quam nam? Reprehenderit culpa, doloremque sequi consequatur officiis voluptatum molestias unde quas.</p>
         </div>
-        <img src="{{ asset('images/advertisement-img.png') }}" alt="Advertisement image">
+        <div class="advertisement">
+            <h5>ADVERTISEMENT</h5>
+            <img src="{{ asset('images/placeholder.com.png') }}" alt="Advertisement image (placeholder instead)">
+        </div>
+    </div>
+    <div class="comic-info">
+        <div class="comic-talent">
+            <h3>Talent</h3>
+            <div class="comic-artist">
+                <h5>Art by:</h5>
+                <span>artisti vari</span>
+            </div>
+            <div class="comic-writers">
+                <h5>Written by:</h5>
+                <span>scrittori vari</span>
+            </div>
+        </div>
+        <div class="comic-specs">
+            <h3>Specs</h3>
+            <div class="comic-series">
+                <h5>Series:</h5>
+                <span>Action Comics</span>
+            </div>
+            <div class="comic-value">
+                <h5>U.S. Price:</h5>
+                <span>$19.99</span>
+            </div>
+            <div class="comic-release">
+                <h5>On Sale Date</h5>
+                <span>Oct 02 2018</span>
+            </div>
+        </div>
+
     </div>
 
 </div>
